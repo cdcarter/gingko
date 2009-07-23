1b5bc565664ca8924a4e79d653d04144eb524e53
Christian Carter <cdcarter@gmail.com>
2009-07-23 02:46:13 -0500

    added stats methods to player/team

--- app/models/player.rb
+++ app/models/player.rb
@@ -5,4 +5,42 @@ class Player < ActiveRecord::Base
   has_many :player_results
   
   #TODO: add stats methods
+  
+  def tuh
+    self.player_results.inject(0) { |m,o| m += o.tuh }
+  end
+  
+  def gp
+    self.player_results.inject(0.to_f) { |m,pr|
+      m += (pr.tuh.to_f / pr.game.team_results.find {|tr| tr.team == self.team }.tuh.to_f)
+    }
+  end
+  
+  def tossups
+    self.player_results.inject(0) {|m,o| m += o.tossups }
+  end
+  
+  def powers
+    self.player_results.inject(0) {|m,o| m += o.powers }
+  end
+  
+  def negs
+    self.player_results.inject(0) {|m,o| m += o.negs }
+  end
+  
+  def points
+    self.player_results.inject(0) {|m,o| m += o.points }
+  end
+  
+  def ppg
+    self.points.to_f / self.gp
+  end
+  
+  def ppth
+    self.points.to_f / self.tuh.to_f
+  end
+  
+  def ppn
+    self.powers.to_f / self.negs.to_f
+  end
 end
--- app/models/team.rb
+++ app/models/team.rb
@@ -13,4 +13,78 @@ class Team < ActiveRecord::Base
   belongs_to :tournament
   
   #TODO: add stats methods
+  
+  def wins
+    self.team_results.select {|tr| tr.win? }.size
+  end
+  
+  def losses
+    self.team_results.select {|tr| !tr.win? }.size
+  end
+  
+  def gp
+    self.team_results.size
+  end
+  
+  def pct
+    self.wins.to_f / self.gp.to_f
+  end
+  
+  def points
+    self.team_results.inject(0) {|m,o| m += o.points }
+  end
+  
+  def ppg
+    self.points.to_f / self.gp.to_f
+  end
+  
+  def points_against
+    self.team_results.inject(0) {|m,tr|
+      tr.game.find {|t| t != self }.points
+    }
+  end
+  
+  def papg
+    self.points_against.to_f / self.gp.to_f
+  end
+  
+  def margin
+    self.ppg - self.papg
+  end
+  
+  def powers
+    self.team_results.inject(0) {|m,o| m += o.powers }
+  end
+  
+  def tossups
+    self.team_results.inject(0) {|m,o| m += o.tossups }
+  end  
+  
+  def negs
+    self.team_results.inject(0) {|m,o| m += o.negs }
+  end
+  
+  def tuh
+    self.team_results.inject(0) {|m,o| m += o.tuh }
+  end
+  
+  def ppth
+    self.points.to_f / self.tuh.to_f
+  end
+  
+  def ppn
+    self.powers.to_f / self.negs.to_f
+  end
+  
+  def bonus_heard
+    self.team_results.inject(0) {|m,o| m += o.bonus_heard }
+  end
+  
+  def bonus_points
+    self.team_results.inject(0) {|m,o| m += o.bonus_points }
+  end
+  
+  def ppb
+    self.bonus_points.to_f / self.bonus_heard.to_f
+  end
 end
